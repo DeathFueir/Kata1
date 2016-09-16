f18f82ab1000567e1dc82c501ceae01a0a69c26c
usuario <usuario@10.22.143.48>
2016-09-16 09:54:27 +0100

    Kata1 quitando comentarios.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,20 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kata1;
 
 import java.util.Date;
 
-/**
- *
- * @author usuario
- */
+
 public class Kata1 {
     
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Person a = new Person("Sergio",new Date(96/05/29));
         System.out.println("Este individuo llamado "+a.getName()+" y tiene "+a.getAge()+" años.");
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,15 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata1;
 
 import java.util.Date;
 
-/**
- *
- * @author usuario
- */
+
 public class Person {
     private final String name;
     private final Date birthday;
